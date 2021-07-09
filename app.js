ad4d344f020ae4f1ea74e5e8aac7388f773d2b6f
Muhammad Raza <mh.raza2906@gmail.com>
2021-07-10 00:46:09 +0100

    Finish build Add transition effect to keypress Remove transition of keypress Add more design changes

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ KEY PRESS EVENT
 =======================
 */
 
-window.addEventListener("keydown", function (e) {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  console.log(key);
+
   audio.currentTime = 0;
   if (!audio) return;
   audio.play();
-});
+
+  key.classList.add("playing");
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  console.log(e.propertyName);
+
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
